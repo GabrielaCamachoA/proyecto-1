8d26664157c034c60d905f18308db658598688d8
Gabriela <gabrielacacosta@gmail.com>
2024-04-12 20:31:15 -0500

    update2

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const dateNumber= document.getElementById("dateNumber");
 const dateText= document.getElementById("dateText");
+const clock= document.getElementById("clock")
 const dateMonth= document.getElementById("dateMonth");
 const dateYear= document.getElementById("dateYear");
 const tastksContainer= document.getElementById("tasksContainer");
@@ -11,4 +12,17 @@ const setDate= () => {
     dateMonth.textContent= date.toLocaleDateString("es",{ month: "short"});
     dateYear.textContent= date.toLocaleDateString("es",{ year: "numeric"});
 }
+
+const setTime= () =>{
+    const date= new Date ();
+    const hours= date.getHours().toString().padStart(2,'0');
+    const minutes= date.getMinutes().toString().padStart(2, '0')
+    const seconds= date.getSeconds().toString().padStart(2, '0')
+    clock.textContent= `${hours}:${minutes}:${seconds}`;
+};
+
+setTime();
 setDate();
+
+/*llamar la constante setTime cada segundo */
+setInterval(setTime, 1000);
